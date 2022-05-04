966546e0fbb07696db3ae2f720eacb57406d63a3
yodaindia <104902384+yodaindia@users.noreply.github.com>
2022-05-04 16:51:01 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(392, music.beat(BeatFraction.Whole))
+    }
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(0)
+})
+radio.setGroup(6)
+basic.forever(function () {
+	
+})
